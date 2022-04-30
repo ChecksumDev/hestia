eede74bef52e22c6590763c9be3815fa14fb4556
Checksum <checksum@velvet.moe>
2022-04-30 03:45:49 -0500

    feat: :sparkles: Generate default configuration file on launches where there is none.

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,8 +1,11 @@
-use std::{fs::File, io::Read};
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
     token: String,
     prefix: String,
@@ -18,7 +21,7 @@ impl Config {
     /// ```
     /// # Panics
     /// If the file could not be read or parsed.
-    pub fn from_toml(path: &str) -> Result<Self, toml::de::Error> {
+    fn from_toml(path: &str) -> Result<Self, toml::de::Error> {
         let config = match File::open(path) {
             Ok(mut file) => {
                 let mut contents = String::new();
@@ -31,9 +34,60 @@ impl Config {
                 toml::from_str::<Config>(contents.as_str())
             }
             Err(why) => {
-                panic!("Could not open config: {}", why);
+                panic!("Failed to open the configuration file: {}", why);
             }
         };
+
+        config
+    }
+
+    /// Generates a config.toml file from a Config struct.
+    /// # Examples
+    /// ```
+    /// use utils::config::Config;
+    /// let config = Config {
+    ///    token: "token".to_string(),
+    ///   prefix: "prefix".to_string(),
+    /// };
+    /// config.to_toml("config.toml");
+    /// ```
+    /// # Panics
+    /// If the file could not be written.
+    fn generate_toml(path: &str) {
+        let mut file = match File::create(path) {
+            Ok(file) => file,
+            Err(why) => panic!("Failed to create the configuration file: {}", why),
+        };
+
+        let contents = toml::to_string::<Config>(&Config::default()).unwrap();
+
+        match file.write_all(contents.as_bytes()) {
+            Ok(_) => (),
+            Err(why) => panic!("Failed to write the default configuration file: {}", why),
+        };
+    }
+
+    /// Returns the current config or a default config if the config file does not exist.
+    /// # Examples
+    /// ```
+    /// use utils::config::Config;
+    /// let config = Config::get_config("config.toml");
+    /// ```
+    /// # Panics
+    /// If the file could not be read or parsed.
+    /// If the file could not be written.
+    pub fn get_config(path: &str) -> Self {
+        // Check if the config.toml file exists
+        if !std::path::Path::new(path).exists() {
+            Config::generate_toml(path);
+        }
+
+        // Read the config.toml file
+        let config = Config::from_toml("config.toml").unwrap();
+
+        if config.token.is_empty() {
+            panic!("\"token\" is a required field in the config.toml file, please change it.");
+        }
 
         config
     }
@@ -54,5 +108,14 @@ impl Config {
     /// ```
     pub fn token(&self) -> &str {
         &self.token
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            token: String::new(),
+            prefix: String::from("~"),
+        }
     }
 }
