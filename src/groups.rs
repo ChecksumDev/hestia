356f57b9f365e820ff66031dc07681857194f843
Checksum <checksum@velvet.moe>
2022-05-01 20:48:46 -0500

    refactor: :construction: Update commands in group

--- src/groups.rs
+++ src/groups.rs
@@ -1,12 +1,19 @@
 use crate::commands::dev::shutdown::*;
-use crate::commands::misc::ping::*;
+use crate::commands::fun::{
+    cat::*, coinflip::*, dadjoke::*, dog::*, eightball::*, roll::*, waifu::*,
+};
+use crate::commands::misc::{ping::*};
 use crate::commands::staff::{ban::*, kick::*, unban::*};
-use crate::commands::user::{avatar::*};
+use crate::commands::user::avatar::*;
 use serenity::framework::standard::macros::group;
 
 #[group]
 #[commands(avatar)]
 struct User;
+
+#[group]
+#[commands(coinflip, dadjoke, roll, eightball, waifu, cat, dog)]
+struct Fun;
 
 #[group]
 #[commands(ping)]
